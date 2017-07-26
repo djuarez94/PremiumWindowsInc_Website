7c8432d76de8a92df6c9a65eb84bc3461e42e60d
David Juarez <davidjuarez1411@gmail.com>
2017-07-26 12:46:52 -0700

    changed header for doors, glass, and windows.html in order to load in the correct css. Also, added a modal slideshow plugin for the glass.html images

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+	/*-------------------------------------
+	| Remove Padding on Modal Click
+	-------------------------------------*/
+	 $('#lightbox2').on('shown.bs.modal', function() {
+	        $("body.modal-open").removeAttr("style");
+			$('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
+	  });
+
+	$('#lightbox2').on('shown', function() {
+	        $("body.modal-open").removeAttr("style");
+			$('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
+	});
   });
