703eff0095d6649b14c7d79afff1b994d73b13b5
David Juarez <davidjuarez1411@gmail.com>
2017-07-26 13:58:53 -0700

    changed css for carousel-caption element text

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 	-------------------------------------*/
 	 $('#lightbox2').on('shown.bs.modal', function() {
 	        $("body.modal-open").removeAttr("style");
-			$('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
+			// $('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
 	  });
 
 	$('#lightbox2').on('shown', function() {
 	        $("body.modal-open").removeAttr("style");
-			$('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
+			// $('div.modal .fade .and .carousel .slide .in').css('padding-right' : '0px');
 	});
   });
